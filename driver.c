5d042fbd86d6d38085fd02b5bdbdf8befd24bec6
Etienne Dechamps <etienne@edechamps.fr>
2022-01-04 22:12:21 +0000

    Print the device name on device add

--- driver.c
+++ driver.c
@@ -2,6 +2,18 @@
 #include <Wdm.h>
 #include <Wdf.h>
 
+static void WinSoftVol_PrintDeviceName(IN WDFDEVICE device) {
+	WDFMEMORY memory;
+	const NTSTATUS queryPropertyStatus = WdfDeviceAllocAndQueryProperty(device, DevicePropertyFriendlyName, PagedPool, WDF_NO_OBJECT_ATTRIBUTES, &memory);
+	if (!NT_SUCCESS(queryPropertyStatus)) {
+		KdPrintEx((DPFLTR_IHVAUDIO_ID, DPFLTR_WARNING_LEVEL, "WinSoftVol: WdfDeviceAllocAndQueryProperty() failed with status 0x%x\n", queryPropertyStatus));
+		return;
+	}
+	// TODO: the memory will live for as long as the driver - we should release it early
+
+	KdPrintEx((DPFLTR_IHVAUDIO_ID, DPFLTR_INFO_LEVEL, "WinSoftVol: attached to device `%S`\n", (wchar_t*)WdfMemoryGetBuffer(memory, /*BufferSize=*/NULL)));
+}
+
 static EVT_WDF_DRIVER_DEVICE_ADD WinSoftVol_EvtWdfDriverDeviceAdd;
 static NTSTATUS WinSoftVol_EvtWdfDriverDeviceAdd(IN WDFDRIVER driver, IN PWDFDEVICE_INIT deviceInit) {
 	UNREFERENCED_PARAMETER(driver);
@@ -14,6 +26,8 @@ static NTSTATUS WinSoftVol_EvtWdfDriverDeviceAdd(IN WDFDRIVER driver, IN PWDFDEV
 		KdPrintEx((DPFLTR_IHVAUDIO_ID, DPFLTR_ERROR_LEVEL, "WinSoftVol: WdfDeviceCreate() failed with status 0x%x\n", deviceCreateStatus));
 		return deviceCreateStatus;
 	}
+
+	WinSoftVol_PrintDeviceName(device);
 	return deviceCreateStatus;
 }
 
