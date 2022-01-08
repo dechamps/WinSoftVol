45a4ca5c9ab4767d16d58bf53de3f639855e9042
Etienne Dechamps <etienne@edechamps.fr>
2022-01-08 15:16:23 +0000

    Intercept KSPROPERTY_AUDIO_VOLUMELEVEL instead of hiding the node

--- driver.c
+++ driver.c
@@ -41,7 +41,7 @@ static void WinSoftVol_ForwardRequest(IN WDFDEVICE device, IN WDFREQUEST request
 	}
 }
 
-static BOOL WinSoftVol_IsGetKsTopologyNodesPropertyRequest(IN WDFREQUEST request) {
+static BOOL WinSoftVol_IsKsAudioVolumelevelRequest(IN WDFREQUEST request) {
 	WDF_REQUEST_PARAMETERS requestParameters;
 	WDF_REQUEST_PARAMETERS_INIT(&requestParameters);
 	WdfRequestGetParameters(request, &requestParameters);
@@ -64,81 +64,9 @@ static BOOL WinSoftVol_IsGetKsTopologyNodesPropertyRequest(IN WDFREQUEST request
 	}
 
 	const KSPROPERTY* const ksProperty = WdfMemoryGetBuffer(inputMemory, /*BufferSize=*/NULL);
-	const BOOL isPropertySetRequest = IsEqualGUID(&ksProperty->Set, &KSPROPSETID_Topology) && ksProperty->Id == KSPROPERTY_TOPOLOGY_NODES && ksProperty->Flags & KSPROPERTY_TYPE_GET;
+	const BOOL isAudioVolumelevelRequest = IsEqualGUID(&ksProperty->Set, &KSPROPSETID_Audio) && ksProperty->Id == KSPROPERTY_AUDIO_VOLUMELEVEL;
 	WdfObjectDelete(inputMemory);
-	return isPropertySetRequest;
-}
-
-static void  WinSoftVol_OnRequestSuccess(IN WDFREQUEST request) {
-	// Note: in theory this is not the correct way to access the output buffer for METHOD_NEITHER I/O.
-	// However, in practice it has been observed that the lower driver is playing a weird game where they don't touch Irp.UserBuffer (which is the normal output buffer for METHOD_NEITHER).
-	// Instead they unilaterally set Irp.AssociatedIrp.SystemBuffer and *that* is the real output buffer. In other words they are changing the rules mid-game and suddenly decide to switch to buffered I/O for the output.
-	// Adding insult to injury, we can't use WdfRequestRetrieveOutputBuffer() because that function will notice we're trying to use it on a METHOD_NEITHER IOCTL and fail validation.
-	// Therefore, we have to get our hands dirty and look at the IRP directly.
-	// See https://community.osr.com/discussion/comment/303718/#Comment_303718 for details.
-	// TODO: it's not clear if all lower drivers would behave like this. We might have to support the standard way as well just in case.
-	const PIRP irp = WdfRequestWdmGetIrp(request);
-	char* const outputBuffer = irp->AssociatedIrp.SystemBuffer;
-	if (outputBuffer == NULL) {
-		WinSoftVol_Log(DPFLTR_ERROR_LEVEL, "Output buffer is NULL!");
-		return;
-	}
-
-	const ULONG outputBufferLength = IoGetCurrentIrpStackLocation(irp)->Parameters.DeviceIoControl.OutputBufferLength;
-	const size_t expectedBufferLength = sizeof(KSMULTIPLE_ITEM);
-	if (outputBufferLength < expectedBufferLength) {
-		WinSoftVol_Log(DPFLTR_ERROR_LEVEL, "Output buffer length is %lu, expected at least %zu\n", outputBufferLength, expectedBufferLength);
-		return;
-	}
-
-	const KSMULTIPLE_ITEM* const ksMultipleItem = irp->AssociatedIrp.SystemBuffer;
-	if (outputBufferLength < ksMultipleItem->Size) {
-		WinSoftVol_Log(DPFLTR_ERROR_LEVEL, "KSMULTIPLE_ITEM size is %lu, but the buffer length is only %lu\n", ksMultipleItem->Size, outputBufferLength);
-		return;
-	}
-
-	const ULONG itemCount = ksMultipleItem->Count;
-	const size_t expectedSize = sizeof(KSMULTIPLE_ITEM) + itemCount * sizeof(GUID);
-	if (ksMultipleItem->Size != expectedSize) {
-		WinSoftVol_Log(DPFLTR_ERROR_LEVEL, "Expected KSMULTIPLE_ITEM size to be %zu for %lu items, got %lu instead\n", expectedSize, itemCount, ksMultipleItem->Size);
-		return;
-	}
-
-	for (ULONG index = 0; index < itemCount; ++index) {
-		GUID* const guid = ((GUID*)(outputBuffer + sizeof(KSMULTIPLE_ITEM))) + index;
-		if (IsEqualGUID(guid, &KSNODETYPE_VOLUME)) {
-			WinSoftVol_Log(DPFLTR_ERROR_LEVEL, "Found KSNODETYPE_VOLUME at node index %lu, replacing with dummy node\n", index);
-			*guid = GUID_NULL;
-		}
-	}
-}
-
-static EVT_WDF_REQUEST_COMPLETION_ROUTINE WinSoftVol_WdfRequestCompletionRoutine;
-static void WinSoftVol_WdfRequestCompletionRoutine(IN WDFREQUEST request, IN WDFIOTARGET ioTarget, IN PWDF_REQUEST_COMPLETION_PARAMS requestCompletionParams, IN WDFCONTEXT context) {
-	UNREFERENCED_PARAMETER(ioTarget);
-	UNREFERENCED_PARAMETER(context);
-
-	if (!NT_SUCCESS(requestCompletionParams->IoStatus.Status)) {
-		WinSoftVol_Log(DPFLTR_INFO_LEVEL, "Request came back with error status 0x%x\n", requestCompletionParams->IoStatus.Status);
-	}
-	else {
-		WinSoftVol_OnRequestSuccess(request);
-	}
-
-	WdfRequestComplete(request, requestCompletionParams->IoStatus.Status);
-}
-
-static BOOL WinSoftVol_InterceptRequest(IN WDFREQUEST request, IN WDFDEVICE device) {
-	WdfRequestFormatRequestUsingCurrentType(request);
-	WdfRequestSetCompletionRoutine(request, WinSoftVol_WdfRequestCompletionRoutine, WDF_NO_CONTEXT);
-	if (!WdfRequestSend(request, WdfDeviceGetIoTarget(device), WDF_NO_SEND_OPTIONS)) {
-		const NTSTATUS requestStatus = WdfRequestGetStatus(request);
-		WinSoftVol_Log(DPFLTR_ERROR_LEVEL, "Intercepting WdfRequestSend() failed with status 0x%x\n", requestStatus);
-		WdfRequestComplete(request, requestStatus);
-		return FALSE;
-	}
-
-	return TRUE;
+	return isAudioVolumelevelRequest;
 }
 
 // We do not use EvtIoDeviceControl to handle IOCTLs.
@@ -146,9 +74,12 @@ static BOOL WinSoftVol_InterceptRequest(IN WDFREQUEST request, IN WDFDEVICE devi
 // See https://community.osr.com/discussion/comment/303709
 static EVT_WDF_IO_IN_CALLER_CONTEXT WinSoftVol_EvtWdfIoInCallerContext;
 static void WinSoftVol_EvtWdfIoInCallerContext(IN WDFDEVICE device, IN WDFREQUEST request) {
-	if (WinSoftVol_IsGetKsTopologyNodesPropertyRequest(request)) {
-		WinSoftVol_Log(DPFLTR_INFO_LEVEL, "Got KS nodes property get request\n");
-		if (WinSoftVol_InterceptRequest(request, device)) return;
+	// Note: an alternative approach that was also attempted is to intercept KSPROPSETID_Topology KSPROPERTY_TOPOLOGY_NODES KSPROPERTY_TYPE_GET requests and remove KSNODETYPE_VOLUME node type GUIDs from the response.
+	// However, that doesn't fool the Windows Audio Engine because it doesn't actually care about the node type; instead, it asks for KSPROPERTY_AUDIO_VOLUMELEVEL on every single node it can find, regardless of node type.
+	if (WinSoftVol_IsKsAudioVolumelevelRequest(request)) {
+		WinSoftVol_Log(DPFLTR_INFO_LEVEL, "Intercepting KSPROPERTY_AUDIO_VOLUMELEVEL request\n");
+		WdfRequestComplete(request, STATUS_NOT_FOUND);
+		return;
 	}
 
 	// According to EVT_WDF_IO_IN_CALLER_CONTEXT docs we're not supposed to do this here.
